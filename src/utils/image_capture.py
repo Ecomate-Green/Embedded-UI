55825d74cd3886b597230d79355b6740ae883ac0
Mohieldin-Hammad <97237706+Mohieldin-Hammad@users.noreply.github.com>
2024-06-25 01:57:13 +0300

    add validation for ImageCapture

--- src/utils/image_capture.py
+++ src/utils/image_capture.py
@@ -4,30 +4,37 @@ import requests
 class ImageCapture:
     def __init__(self, device_index=0):
         self.capture = cv2.VideoCapture(device_index)
+        if not self.capture.isOpened():
+            raise Exception("Could not open video device")
 
     def read_frame(self):
         ret, frame = self.capture.read()
         return ret, frame
 
     def send_image_to_server(self, image, url, token, api_key):
-        resized_image = cv2.resize(image, (256, 256))
-        _, img_encoded = cv2.imencode('.jpg', resized_image, [int(cv2.IMWRITE_JPEG_QUALITY), 50])  # High compression
-        # Check file size in bytes
-        # print(len(img_encoded.tobytes()))
+        try:
+            resized_image = cv2.resize(image, (256, 256))
+            _, img_encoded = cv2.imencode('.jpg', resized_image, [int(cv2.IMWRITE_JPEG_QUALITY), 50])  # High compression
+            # Check file size in bytes
+            # print(len(img_encoded.tobytes()))
 
-        # # If you want to send the token as bearer
-        # headers = {'Authorization': f'Bearer {token}'}
-        # response = requests.post(url, files={"file": img_encoded.tobytes()}, headers=headers)
+            # # If you want to send the token as bearer
+            # headers = {'Authorization': f'Bearer {token}'}
+            # response = requests.post(url, files={"file": img_encoded.tobytes()}, headers=headers)
 
-        params = { 
-            "machine_token": token, 
-            "api_key": api_key
-        }
-        files = {
-            'image': ('image.jpg', img_encoded.tobytes(), 'image/jpeg')
-        }
-        response = requests.post(url, params=params, files=files)
-        return response
+            params = { 
+                "machine_token": token, 
+                "api_key": api_key
+            }
+            files = {
+                'image': ('image.jpg', img_encoded.tobytes(), 'image/jpeg')
+            }
+            response = requests.post(url, params=params, files=files)
+            response.raise_for_status()  # Raise an error for bad responses
+            return response
+        except Exception as e:
+            print(f"An error occurred while sending the image: {e}")
+            return None
 
     def release(self):
         self.capture.release()
