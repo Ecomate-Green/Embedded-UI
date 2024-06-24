c0527def8c2b1fe51b9138c1c87423789c9af05a
Mohieldin-Hammad <97237706+Mohieldin-Hammad@users.noreply.github.com>
2024-06-24 06:51:48 +0300

    fix: the token inside the qr code is not refreshing every new confirmation

--- touch_screen.py
+++ touch_screen.py
@@ -103,17 +103,19 @@ class ScanScreen(Screen):
         qr_image.save(qr_image_path)
         # Set QR code image source in the Kivy Image widget
         self.ids.qr_code_image.source = qr_image_path
+        self.ids.qr_code_image.reload()  # Ensure the image is reloaded
 
 
 
 class SignUpScreen(Screen):
+    textbox = ObjectProperty(None)
+    
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.token = ""
         self.url = f"{os.environ.get('SERVER_URL')}/api/v1/transaction/confirm"
         self.api_key = os.environ.get("API_KEY")
 
-    textbox = ObjectProperty(None)
 
     def set_transaction_token(self, token):
         self.token = token
@@ -151,9 +153,19 @@ class SignUpScreen(Screen):
             # # Debugging
             # print("Response status code:", response.status_code)
             # print("Response headers:", response.headers)
-            print("Response content:", response.content)
+            print("Response content:", response.json())
         except Exception as e:
             print(f"An error occurred: {e}")
+            # Here you might want to provide feedback to the user in the UI
+            self.show_error_popup(str(e))
+
+    def show_error_popup(self, message):
+        from kivy.uix.popup import Popup
+        from kivy.uix.label import Label
+        popup = Popup(title='Error',
+                      content=Label(text=message),
+                      size_hint=(None, None), size=(400, 200))
+        popup.open()
         
 
     
